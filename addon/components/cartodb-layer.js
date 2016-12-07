a13addc454116b2be66619404c0f34b52fea9c3c
Matt Gardner <wilburnforce@gmail.com>
2016-12-07 18:37:02 -0500

    Adding viz json config information to leaflet map object

--- addon/components/cartodb-layer.js
+++ addon/components/cartodb-layer.js
@@ -26,9 +26,9 @@ export default BaseLayer.extend({
     if (this.get('containerLayer')) {
       let map = this.get('containerLayer')._layer;
       let zIndex = this.get('options.zIndex');
-
       this._layer.on('done', (layer) => {
         cdb.geo.LeafletMapView.addLayerToMap(layer, map, zIndex);
+        Ember.set(map, 'vizJson', Ember.get(layer, 'options.options.layer_definition.layers'));
         this.layer = layer;
         this.didCreateLayer();
         if(this.get('sql')) {
